fd7f4d2e023015150586223a1bb3e7f0121068b2
M R N GOPI KRISHNA <gopi.muktevi@gmail.com>
2024-12-17 21:30:58 +0530

    adding more routes to auth
    Signed-off-by: M R N GOPI KRISHNA <gopi.muktevi@gmail.com>

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,9 +1,15 @@
 "use client";
+import { useAuth } from "@clerk/nextjs";
 import { useState } from "react";
 
 export const Counter = () => {
-    console.log("Counter Component");
+
+    const { isLoaded, userId } = useAuth();
     const [count, setCount] = useState(0);
+
+    if (!isLoaded || !userId) {
+        return null;
+    }
 
     return(
         <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
